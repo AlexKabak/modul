71a6c2cb16d7943f874225cdea4c535f4b4697ab
AlexKabak <kabak.o@donnu.edu.ua>
2018-03-24 13:27:28 +0200

    v1

---
+++ mod/js/main.js
@@ -0,0 +1,23 @@
+
+$(".pink").mouseenter( function (){
+	$(".triug1").css("visibility", "visible");
+}
+);
+$(".pink").mouseenter( function (){
+	$(".triug2").css("visibility", "visible");
+}
+);
+$(".red").mouseenter( function (){
+	$(".triug1").addClass("loader");
+}
+);
+$(".red").mouseenter( function (){
+	$(".triug2").addClass("loader1");
+}
+);
+$(".red").mouseenter( function (){
+	$(".triug1").animate({marginLeft: "0px"})
+});
+$(".red").mouseenter( function (){
+	$(".triug2").animate({marginLeft: "0px"})
+});
